7b2a0e22f0f273c3f3c634e4bbdd57882d252b19
tramp <wanggang3@staff.sina.com.cn>
2020-05-09 15:36:10 +0800

    update => 初始化项目

---
+++ RTPhotoPicker/RTPhotoPicker.h
@@ -0,0 +1,19 @@
+//
+//  RTPhotoPicker.h
+//  RTPhotoPicker
+//
+//  Created by tramp on 2020/5/9.
+//  Copyright © 2020 tramp. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RTPhotoPicker.
+FOUNDATION_EXPORT double RTPhotoPickerVersionNumber;
+
+//! Project version string for RTPhotoPicker.
+FOUNDATION_EXPORT const unsigned char RTPhotoPickerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RTPhotoPicker/PublicHeader.h>
+
+
